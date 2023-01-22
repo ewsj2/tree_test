56e7ace5c568d980918d0c8ddea2afa3fb864f1b
Emma Jones <ewsj2@cam.ac.uk>
2023-01-22 11:31:27 +0000

    inital commit

---
+++ tree_test.py
@@ -0,0 +1,8 @@
+from ete3 import Tree, TreeStyle
+t = Tree()
+t.populate(10, random_dist=True)
+ts=TreeStyle()
+ts.show_leaf_name=True
+ts.show_branch_length=True
+ts.show_branch_support=True
+t.show(tree_style=ts)
